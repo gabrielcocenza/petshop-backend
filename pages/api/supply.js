21639d31b19b3cf83923eb37a9573bf6b63aef50
cocenza <gabrielcocenza@gmail.com>
2020-12-05 21:30:30 -0300

    fix delete cors supply

--- pages/api/supply.js
+++ pages/api/supply.js
@@ -10,7 +10,7 @@ const cors = initMiddleware(
     // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
     Cors({
         // Only allow requests with GET, POST and OPTIONS
-        methods: ['GET', 'POST', 'OPTIONS', 'PATCH'],
+        methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
 
     })
 )
